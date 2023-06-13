d57f9c94fc946c021f271c80f6593a791f180a80
Hayan Butt <hayan_880@hotmail.com>
2023-06-13 16:05:52 +0100

    can see question modal when button is clicked

--- src/containers/PaintingListContainer.js
+++ src/containers/PaintingListContainer.js
@@ -3,7 +3,7 @@ const PaintingListContainer = ({questionBeingDisplayed}) => {
         <div className="caught-fish">
 
         <h3> This is the PaintingListContainer</h3>   
-        {questionBeingDisplayed}
+        
         </div>
      );
 }
