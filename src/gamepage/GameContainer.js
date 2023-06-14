780c319d2c2ec24b8dfb3b0987d3f82a759b752a
Isabel <isabelgalwey@gmail.com>
2023-06-14 11:31:50 +0100

    started painting display modification

--- src/gamepage/GameContainer.js
+++ src/gamepage/GameContainer.js
@@ -28,6 +28,7 @@ const GameContainer = ({updateArtworkInGame, updateGame, activePlayer, currentGa
   const displayPaintingInfo = (index) => {
     // console.log("Displays modal for Multiple Choice Question / Displays info about painting, giving the option for the player to select this painting");
     // console.log(`${artworksInGame[index].title}, ${artworksInGame[index].artist}`);
+    if (artworksInGame[index].stolen)
     setPaintingInfo(<>{artworksInGame[index].artwork.title}, {artworksInGame[index].artwork.artist}<br/>£{artworksInGame[index].artwork.value}<br/>{artworksInGame[index].artwork.rarityLevel.substring(0, 1) + artworksInGame[index].rarityLevel}</>);
     setDisplayPaintingInfoStatus("visible");
     setCurrentArtworkInGame(artworksInGame[index]);
