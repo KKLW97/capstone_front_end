86f9a7bce171454ba6e3018aedb493ffd5181ad5
Kelly Wong <122545825+KKLW97@users.noreply.github.com>
2023-06-15 01:50:06 +0100

    adds alarm glow/setOff CSS

--- src/gamepage/PenaltyList.js
+++ src/gamepage/PenaltyList.js
@@ -1,62 +1,31 @@
-import PenaltyComponent from "./PenaltyComponent";
-import "../CSSfiles/PenaltyList.css"
-const PenaltyList = ({currentGame}) => {
+import React, { useEffect, useState } from "react";
+import "../CSSfiles/PenaltyList.css";
+import alarm from "../assets/alarm.png"
 
-    // const setOffAlarm = () =>{
-        if(currentGame.penalty == 0 ){
-            return ( 
-                <div className="penalty-list-container"> 
-                <section className="penalty-list">
-                    <PenaltyComponent id="alarm1" className="alarm-off"/> 
-                    <PenaltyComponent id="alarm2" className="alarm-on"/>
-                    <PenaltyComponent id="alarm3" className="alarm-off"/>
-                </section>
-                </div>
-             );
-        }else if(currentGame.penalty == 1 ){
-            return ( 
-                <div className="penalty-list-container"> 
-                <section className="penalty-list">
-                    <PenaltyComponent id="alarm1" className="alarm-on"/> 
-                    <PenaltyComponent id="alarm2" className="alarm-off"/>
-                    <PenaltyComponent id="alarm3" className="alarm-off"/>
-                </section>
-                </div>
-             );
-        }else if(currentGame.penalty == 2 ){
-            return ( 
-                <div className="penalty-list-container"> 
-                <section className="penalty-list">
-                    <PenaltyComponent id="alarm1" className="alarm-on"/> 
-                    <PenaltyComponent id="alarm2" className="alarm-on"/>
-                    <PenaltyComponent id="alarm3" className="alarm-off"/>
-                </section>
-                </div>
-             );
-        }else if(currentGame.penalty == 3 ){
-            return ( 
-                <div className="penalty-list-container"> 
-                <section className="penalty-list">
-                    <PenaltyComponent id="alarm1" className="alarm-on"/> 
-                    <PenaltyComponent id="alarm2" className="alarm-on"/>
-                    <PenaltyComponent id="alarm3" className="alarm-on"/>
-                </section>
-                </div>
-             );
-        }
-    }    
 
-    // return ( 
-    //     <div className="penalty-list-container"> 
-    //     <section className="penalty-list">
-    //         {currentGame.penalty == 0 ? <> <PenaltyComponent id="alarm1" className="alarm-off"/> <PenaltyComponent id="alarm2" className="alarm-off"/><PenaltyComponent id="alarm3" className="alarm-off"/></>
-    //         : currentGame.penalty == 1 ? <> <PenaltyComponent id="alarm1" className="alarm-on"/> <PenaltyComponent id="alarm2" className="alarm-on"/><PenaltyComponent id="alarm3" className="alarm-off"/></> 
-    //         : currentGame.penalty==2 ? <> <PenaltyComponent id="alarm1" className="alarm-on"/> <PenaltyComponent id="alarm2" className="alarm-on"/><PenaltyComponent id="alarm3" className="alarm-off"/></>
-    //         : currentGame.penalty==3 ? <> <PenaltyComponent id="alarm1" className="alarm-on"/> <PenaltyComponent id="alarm2" className="alarm-on"/><PenaltyComponent id="alarm3" className="alarm-on"/></>
-    //         :null}
-    //     </section>
-    //     </div>
-    //  );
-// }
- 
+const PenaltyList = ({ currentGame }) => {
+  const [currentPenalty, setCurrentPenalty] = useState(0);
+
+  useEffect(() => {
+    setCurrentPenalty(currentGame.penalty);
+  }, [currentGame]);
+
+  // Determine the number of PenaltyComponent instances to display
+  const numPenalties = 3;
+
+  // Generate an array of indices for rendering PenaltyComponent instances
+  const penaltyIndices = Array.from({ length: numPenalties }, (_, index) => index);
+
+  return (
+    <div className="penalty-list-container">
+      <section className="penalty-list">
+        {penaltyIndices.map((index) => (
+              <img id={index < currentPenalty ? "alarm-on" : "alarm"}
+               src={alarm}  width={100} height={100} alt="an image of security alarm"/>
+        ))}
+      </section>
+    </div>
+  );
+};
+
 export default PenaltyList;
