bcbc5addaac665f68afd52b6faba11ddc5ddae60
Kelly Wong <122545825+KKLW97@users.noreply.github.com>
2023-06-15 15:41:28 +0100

    adds css to new player title and refactors showhide function

--- src/homepage/StoryComponent.js
+++ src/homepage/StoryComponent.js
@@ -2,38 +2,49 @@ import { useState } from "react";
 
 const StoryComponent = () => {
 
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClick = () => {setShow((prev) => !prev)}
+  const handleClick = () => {setShow((prev) => !prev)}
  
-        return (
+  return (
+      
+
+    <div>
+        
+
+    {show==false ? 
+            <div>
+                <p>
+                  <b>Time:</b> Tonight
+                  <br/>
+                  <b>Location:</b> BNTA Gallery
+                  <br/>
+                  <b>Event:</b>The biggest art heist in history...  
+                  <button className="story-button" onDoubleClick={handleClick}>Learn More</button>
+                </p>
+            </div>
+        :
+        <div>
+          <p>
+                  <b>Time:</b> Tonight
+                  <br/>
+                  <b>Location:</b> BNTA Gallery
+                  <br/>
+                  <b>Event:</b>The biggest art heist in history! 
+                  <br/>
+                  You are Stephane, a famous art thief. 
+                  <br/>
+                  Tonight you and your cat will break into the heavily guarded BNTA Gallery.
+                  <br/>
+                  Login if you dare... 
+                <button className="story-button" onDoubleClick={handleClick}>Back</button></p>
+        </div>}
+    
+    </div>
             
 
-                <div>
-                   
-
-                {show==false ? 
-                        <div>
-                            <p>
-                            <b>Time:</b> Tonight
-                            <br/>
-                            <b>Location:</b> BNTA Gallery
-                            <br/>
-                            <b>Event:</b>The biggest art heist in history...  
-                            <button onDoubleClick={handleClick}>Learn More</button>
-                            </p>
-                        </div>
-                    :
-                    <div>
-                            You are Stephane, a famous art thief. Tonight you and your cat will break into the heavily guarded BNTA Gallery. Login if you dare... 
-                            <button onDoubleClick={handleClick}>Back</button>
-                    </div>}
-                
-                </div>
-                
-
-        );
-      };
+  );
+};
 
 
 
