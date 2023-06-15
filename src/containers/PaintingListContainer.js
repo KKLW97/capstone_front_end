6368b6dd62ccc852249b89f70c1c370da84876af
Hayan Butt <hayan_880@hotmail.com>
2023-06-15 09:45:13 +0100

    score on list updates

--- src/containers/PaintingListContainer.js
+++ src/containers/PaintingListContainer.js
@@ -1,9 +1,9 @@
-const PaintingListContainer = ({stolenArtworkList, questionBeingDisplayed}) => {
+const PaintingListContainer = ({stolenArtworkList, questionBeingDisplayed, currentGame}) => {
     return ( 
         <div className="caught-fish">
             <h3> This is the PaintingListContainer</h3>
             <h3>Total Score: {currentGame.score}</h3>
-            {stolenArtworkList.map(stolenArtwork => <div><img src={stolenArtwork.artwork.url} height="100px"/><p>"{stolenArtwork.artwork.title}", {stolenArtwork.artwork.artist}</p></div>)
+            {stolenArtworkList.map(stolenArtwork => <div><img src={stolenArtwork.artwork.url} height="100px"/><p>"{stolenArtwork.artwork.title}", {stolenArtwork.artwork.artist}</p></div>)}
         </div>
      );
 }
