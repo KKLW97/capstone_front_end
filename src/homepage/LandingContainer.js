782378af5d727eabd10f525ac46bac6d1c4e7e1e
Kelly Wong <122545825+KKLW97@users.noreply.github.com>
2023-06-15 15:19:13 +0100

    adds footer

--- src/homepage/LandingContainer.js
+++ src/homepage/LandingContainer.js
@@ -19,7 +19,23 @@ const LandingContainer = ({ postNewPlayer}) => {
 
         <h1> This is the landing page</h1>
         
-        
+        <div className="footer">
+            <ul className="footer-nav-list">
+                <li className="">
+                <div className="nav-text">
+                <a href="http://localhost:3000" className="#">Home</a></div>
+                </li>
+                <li className="">
+                <div className="nav-text">
+                <a href="http://localhost:3000/playerAccount" className="#">Account</a></div>
+                </li>
+                <li className="">
+                <div className="nav-text">
+                <a href="http://localhost:3000/gamePage" className="#">Game</a></div>
+                </li>
+            </ul>
+        </div>
+            
         </>
 
      );
